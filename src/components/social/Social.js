2909c7e94ab80ff9ae896bd3b96fa151bfe6de70
William Thorne <williamcthorne@gmail.com>
2021-12-19 16:24:34 +0100

    realized I needed to use React Hook useState to make hover effect in css

--- src/components/social/Social.js
+++ src/components/social/Social.js
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './social.css';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faTwitter, faYoutube, faTwitch, faInstagram} from '@fortawesome/free-brands-svg-icons'
-import {FaYoutube} from'react-icons/fa';
-import {IconContext} from 'react-icons';
+
 
 
 const Social = () => {
+
+    const [over, setOver] = useState(false);
     return (
-        <div className="soup__social section" style={{display:'flex', alignItems:'center', justifyContent:'center', cursor:'pointer', }}>
+        <div className="soup__social section" 
+            style={
+                {display:'flex', 
+                alignItems:'center', 
+                justifyContent:'center', 
+                cursor:'pointer', }}>
             
+            {/* onMouseOver={()=> setOver(true)}
+            onMouseLeave={()=> setOver(false)} */}
+
             <FontAwesomeIcon className="fa-icon" icon={faYoutube} >
             </FontAwesomeIcon>
             <FontAwesomeIcon className="fa-icon"icon={faInstagram} >
